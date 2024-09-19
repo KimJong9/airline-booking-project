b0b2ed666771c2d4c2195c83771944c0fa0383a7
종구 <138adventure@gmail.com>
2024-09-19 18:06:58 +0900

    front git action fix v11

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',  // Babel로 변환 설정
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     },
     transformIgnorePatterns: [
-        '/node_modules/(?!axios)/',  // axios를 변환 대상에서 제외하지 않음
+        '/node_modules/(?!axios)/',  // axios를 Jest에서 변환 대상으로 포함
     ],
+    moduleNameMapper: {
+        '\\.(css|less)$': 'identity-obj-proxy',  // CSS 파일들을 무시하거나 모킹 처리
+    },
+    testEnvironment: 'jsdom',  // React 컴포넌트 테스트를 위해 jsdom 환경 사용
 };
