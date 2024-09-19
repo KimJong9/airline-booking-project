8260dc1a2063968f03dec1f12c563bae6a8149cc
종구 <138adventure@gmail.com>
2024-09-19 17:57:27 +0900

    front git action fix v9

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,11 +2,11 @@ module.exports = {
     presets: [
         '@babel/preset-env',
         '@babel/preset-react'
-    ],
-    transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',  // Babel로 변환
-    },
-    transformIgnorePatterns: [
-        '/node_modules/(?!axios)/'  // 'axios'를 변환하도록 제외
     ]
+    // transform: {
+    //     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',  // Babel로 변환
+    // },
+    // transformIgnorePatterns: [
+    //     '/node_modules/(?!axios)/'  // 'axios'를 변환하도록 제외
+    // ]
 };
