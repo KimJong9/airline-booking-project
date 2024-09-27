1311904fdd4d28c20bc64eb35f8670a14417324a
종구 <138adventure@gmail.com>
2024-09-27 17:42:01 +0900

    db change

--- airline.sql
+++ airline.sql
@@ -13,8 +13,8 @@ CREATE TABLE country_city (
 
 -- 공항 정보 테이블
 CREATE TABLE airport (
-    airport_id SERIAL PRIMARY KEY,          -- 숫자 기반의 고유 식별자
-    airport_name VARCHAR(255) NOT NULL,
+    airport_id SERIAL,          -- 숫자 기반의 고유 식별자
+    airport_name VARCHAR(255) PRIMARY KEY,
     city_name VARCHAR(255) NOT NULL,
     country_name VARCHAR(255) NOT NULL,
     FOREIGN KEY (country_name, city_name) REFERENCES country_city(country_name, city_name) ON DELETE CASCADE
@@ -23,8 +23,8 @@ CREATE TABLE airport (
 -- 항공 정보 테이블
 CREATE TABLE flight (
     flight_code VARCHAR(255) PRIMARY KEY,  -- 고유 비행 코드
-    departure_airport INT NOT NULL,     -- 출발 공항 ID
-    destination_airport INT NOT NULL,   -- 도착 공항 ID
+    departure_airport varchar(255) NOT NULL,     -- 출발 공항 ID
+    destination_airport varchar(255) NOT NULL,   -- 도착 공항 ID
     departure_date DATE NOT NULL,          -- 출발 날짜
     departure_time TIME NOT NULL,          -- 출발 시간
     arrival_date DATE NOT NULL,            -- 도착 날짜
