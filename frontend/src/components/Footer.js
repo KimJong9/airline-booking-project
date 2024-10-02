18bf3fda3a71b3d50628b8bef1c7302c231df638
종구 <138adventure@gmail.com>
2024-10-02 11:50:28 +0900

    test

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
             <p>© 2024 JAWS. 항공 예약 팀 프로젝트...</p>
             <ul>
                 <li><a href="https://example.com">Github Site</a></li>
-                <li><a href="https://example.com">개인정보 처리방침</a></li>
+                <li><a href="https://example.com">개인정보 처리방침2</a></li>
                 <li><a href="https://example.com">문의하기</a></li>
             </ul>
         </footer>
